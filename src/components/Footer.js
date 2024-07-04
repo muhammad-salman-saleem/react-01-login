b8130f771b034aec012e7e7fd9fc8824e79956fc
muhammad-salman-saleem <msalman1221998@gmail.com>
2024-07-04 18:24:47 +0500

    react-01-login

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => (
+  <footer className="bg-light p-3 text-center">
+    <p>
+      Sample project provided by <strong>Muhammad Salman</strong> 
+    </p>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    <p className="lead">
+      Simple Project
+    </p>
+  </div>
+);
+
+export default Hero;
